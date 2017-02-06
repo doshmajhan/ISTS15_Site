78422e55c6d11be0f63f0997ad88d3db723eb77f
Ubuntu <ubuntu@ip-172-31-54-30.ec2.internal>
2017-02-06 02:25:23 +0000

    Added code for adding/removing ally, declaring war and checking if 2 countries are a allies.

--- backend.py
+++ backend.py
@@ -50,6 +50,9 @@ def add_ally():
     cur.execute("INSERT INTO relations (atpeace%s) VALUES (1) WHERE cid=%s" % (cid1, cid2))
     db.commit()
 
+    return 200
+
+
 @app.route('/removeally', methods=['POST'])
 def remove_ally():
     """
@@ -59,19 +62,93 @@ def remove_ally():
         :returns status: The status of the request, 200 for OK, 500+ if error
     """
     args = flask.request.args
+    country1 = args['country1']
+    country2 = args['country2']
+    cur = db.cursor()
+
+    cur.execute("SELECT cid FROM users WHERE user = %s" % (country1))
+    cid1 = cur.fetchone()
+    cur.execute("SELECT cid FROM users WHERE user = %s" % (country2))
+    cid2 = cur.fetchone()
+    
+    cur.execute("INSERT INTO relations (atpeace%s) VALUES (0) WHERE cid=%s" % (cid2, cid1))
+    cur.execute("INSERT INTO relations (atpeace%s) VALUES (0) WHERE cid=%s" % (cid1, cid2))
+    db.commit()
+
+    return 200
 
 
-@app.route('/check_ally', methods=['POST'])
-def check_ally():
+@app.route('/removeally', methods=['POST'])
+def declare_war():
+    """
+        Handles request for declaring war on a team
+        first form element should be the requesting country
+
+        :returns status: The status of the request, 200 for OK, 500+ if error
+    """
+    args = flask.request.args
+    country1 = args['country1']
+    country2 = args['country2']
+    cur = db.cursor()
+
+    cur.execute("SELECT cid FROM users WHERE user = %s" % (country1))
+    cid1 = cur.fetchone()
+    cur.execute("SELECT cid FROM users WHERE user = %s" % (country2))
+    cid2 = cur.fetchone()
+    
+    cur.execute("INSERT INTO relations (atwar%s) VALUES (0) WHERE cid=%s" % (cid2, cid1))
+    cur.execute("INSERT INTO relations (atwar%s) VALUES (0) WHERE cid=%s" % (cid1, cid2))
+    db.commit()
+
+    return 200
+
+
+def check_ally(country1, country2):
     """
         Checks if two countries are allies when a new code for a resource
         is added, to determine if the resource is shared or stolen.
 
+        :param country1: one of the country whose adding a resource
+        :param country2: the country the resource belongs to
         :return status: If the countries are allies or not, 1 if yes, 0 if no
     """
+    cur  = db.cursor()
+    cur.execute("SELECT cid FROM users WHERE user = %s" % (country1))
+    cid1 = cur.fetchone()
+    cur.execute("SELECT cid FROM users WHERE user = %s" % (country2))
+    cid2 = cur.fetchone()
+
+    cur.execute("SELECT atpeace%s FROM relations WHERE cid=%s" % (country1, country2))
+    check1 = cur.fetchone()
+    cur.execute("SELECT atpeace%s FROM relations WHERE cid=%s" % (country2, country1))
+    check2 = cur.fetchone()
+
+    if int(check1) == 1 and int(check2) == 1:
+        return 1
+    else:
+        return 0
+
+
+def check_resource(resource):
+    """
+        Checks which country the resource belongs to
+
+        :param resource: the code for that specific resource
+        :return country: the country the resource belongs to
+    """
+    cur = db.cursor()
+
+
+
+@app.route('/addresource', methods['POST'])
+def add_resource():
+    """
+        Handles adding a new resource to a country
+
+        :return status: status of the request, 200 for OK, 500+ for error
+    """
     args = flask.request.args
-
-
+    country
 if __name__ == '__main__':
     db = connect_db()
     app.run()
