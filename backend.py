9c08b310032b294aff7ae7208dc3a0b8149197df
Doshmajhan <cpc1007@g.rit.edu>
2017-02-06 20:09:23 -0500

    Fixing some lines in add_ally/remove_ally, finishing up remove_ally and add_resource, then need to test

--- backend.py
+++ backend.py
@@ -5,6 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 
 def connect_db():
+    """
+        Create our connection to the mysql database
+
+        :return db: the object for our database connection
+    """
     db = MySQLdb.connect(host='localhost', user='root', 
                          passwd='con162ess', db='ists')
     return db
@@ -31,19 +36,27 @@ def check_ally(country1, country2):
         Checks if two countries are allies when a new code for a resource
         is added, to determine if the resource is shared or stolen.
 
-        :param country1: one of the country whose adding a resource
-        :param country2: the country the resource belongs to
+        :param country1: one of the country whose adding a resource, accept country or cid
+        :param country2: the country the resource belongs to, accepts country or cid
         :return status: If the countries are allies or not, 1 if yes, 0 if no
     """
     cur  = db.cursor()
-    cur.execute("SELECT cid FROM users WHERE user = %s" % (country1))
-    cid1 = cur.fetchone()
-    cur.execute("SELECT cid FROM users WHERE user = %s" % (country2))
-    cid2 = cur.fetchone()
 
-    cur.execute("SELECT atpeace%s FROM relations WHERE cid=%s" % (country1, country2))
+    if isinstance(country1, int):
+        cid1 = country1
+    else:
+        cur.execute("SELECT cid FROM users WHERE user = %s" % (country1))
+        cid1 = cur.fetchone()
+    
+    if isinstance(country2, int):
+        cid2 = country2
+    else:
+        cur.execute("SELECT cid FROM users WHERE user = %s" % (country2))
+        cid2 = cur.fetchone()
+
+    cur.execute("SELECT atpeace%s FROM relations WHERE cid=%s" % (cid1, cid2))
     check1 = cur.fetchone()
-    cur.execute("SELECT atpeace%s FROM relations WHERE cid=%s" % (country2, country1))
+    cur.execute("SELECT atpeace%s FROM relations WHERE cid=%s" % (cid2, cid1))
     check2 = cur.fetchone()
 
     if int(check1) == 1 and int(check2) == 1:
@@ -104,15 +117,21 @@ def add_ally():
     """
     args = flask.request.args
     session = args['session']
-    cid1 = authenticate(session)
+    cid = authenticate(session)
+    if not cid:
+        return 401 # bad auth
+
     country_to_add = args['country']
     cur = db.cursor()
 
     cur.execute("SELECT cid FROM users WHERE user = %s" % (country))
     cid2 = cur.fetchone()
     
-    cur.execute("INSERT INTO relations (atpeace%s) VALUES (1) WHERE cid=%s" % (cid2, cid1))
-    cur.execute("INSERT INTO relations (atpeace%s) VALUES (1) WHERE cid=%s" % (cid1, cid2))
+    cur.execute("INSERT INTO relations (atpeace%s) VALUES (1) WHERE cid=%s" % (cid2, cid))
+    
+    # unsure if i want to automatically make both teams allies if only one of them adds the other
+    # maybe make it so they both have to add each other
+    #cur.execute("INSERT INTO relations (atpeace%s) VALUES (1) WHERE cid=%s" % (cid, cid2))
     db.commit()
 
     return 200
@@ -127,23 +146,27 @@ def remove_ally():
         :returns status: The status of the request, 200 for OK, 500+ if error
     """
     args = flask.request.args
-    country1 = args['country1']
-    country2 = args['country2']
+    session = args['session']
+    cid = authenticate(session)
+    if not cid:
+        return 401 # bad auth
+
+    country_to_remove = args['country2']
     cur = db.cursor()
 
-    cur.execute("SELECT cid FROM users WHERE user = %s" % (country1))
-    cid1 = cur.fetchone()
-    cur.execute("SELECT cid FROM users WHERE user = %s" % (country2))
+    cur.execute("SELECT cid FROM users WHERE user = %s" % (country_to_remove))
     cid2 = cur.fetchone()
     
-    cur.execute("INSERT INTO relations (atpeace%s) VALUES (0) WHERE cid=%s" % (cid2, cid1))
-    cur.execute("INSERT INTO relations (atpeace%s) VALUES (0) WHERE cid=%s" % (cid1, cid2))
+    cur.execute("INSERT INTO relations (atpeace%s) VALUES (0) WHERE cid=%s" % (cid2, cid))
+    
+    #cur.execute("INSERT INTO relations (atpeace%s) VALUES (0) WHERE cid=%s" % (cid, cid2))
     db.commit()
 
+    # add the removal of the resources that they two teams share as well
     return 200
 
 
-@app.route('/removeally', methods=['POST'])
+@app.route('/declarewar', methods=['POST'])
 def declare_war():
     """
         Handles request for declaring war on a team
@@ -152,17 +175,19 @@ def declare_war():
         :returns status: The status of the request, 200 for OK, 500+ if error
     """
     args = flask.request.args
-    country1 = args['country1']
-    country2 = args['country2']
+    session = args['session']
+    cid = authenticate(session)
+    if not cid:
+        return 401 # bad auth
+
+    country_to_attack = args['country2']
     cur = db.cursor()
 
-    cur.execute("SELECT cid FROM users WHERE user = %s" % (country1))
-    cid1 = cur.fetchone()
-    cur.execute("SELECT cid FROM users WHERE user = %s" % (country2))
+    cur.execute("SELECT cid FROM users WHERE user = %s" % (country_to_attack))
     cid2 = cur.fetchone()
     
-    cur.execute("INSERT INTO relations (atwar%s) VALUES (0) WHERE cid=%s" % (cid2, cid1))
-    cur.execute("INSERT INTO relations (atwar%s) VALUES (0) WHERE cid=%s" % (cid1, cid2))
+    cur.execute("INSERT INTO relations (atwar%s) VALUES (1) WHERE cid=%s" % (cid2, cid))
+    cur.execute("INSERT INTO relations (atwar%s) VALUES (1) WHERE cid=%s" % (cid, cid2))
     db.commit()
 
     return 200
@@ -176,13 +201,24 @@ def add_resource():
         :return status: status of the request, 200 for OK, 500+ for error
     """
     args = flask.request.args
+    session = args['session']
+    cid = authenticate(session)
+    if not cid:
+        return 401 # bad auth
+
     country = args['country']
     resource = args['resource']
     resource_owner, resource_type = check_resource(resource)
     if not resource_owner or not resource_type:
         return 503 # bad resource code
     
+    status = check_ally(country, resource_owner)
+    if status:
+        # add resource to database here
+    else:
+        # add resource to database of the one who entered it, remove it from the one who owns it
 
+    return 200
 
 
 if __name__ == '__main__':
