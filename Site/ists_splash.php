a8c1618a40753b2f288d5bc986c29ecf20df4829
Aaron Liu <adl6585@rit.edu>
2017-02-27 14:58:31 -0500

    small content that will need updating later with pics

--- Site/ists_splash.php
+++ Site/ists_splash.php
@@ -6,17 +6,43 @@
 		<div id="splash_button1">
 			<a class="fancybox" href="#quick_tour"><button class="ctrbtn">Quick Tour</button></a>
 		</div>
-		<div id="splash_button2">	
+		<div id="splash_button2">
 			<a class="fancybox" href="#about"><button class="ctrbtn">About <?php echo $team_settings['name']; ?></button></a>
 		</div>
 		<div id="quick_tour" style="display:none;width:500px;">
-			<h1>Example Text</h1>
-			More text here i am
+			<h1>Jump into Russia</h1>
+			Fun Fact about Russia:
+			Each Russian consume 18 litres (4.8 US gal) of alcohol per year, doubling what experts consider dangerous.
+			Another fact* Beer was not considered an alcoholic beverage in Russia until 2013
+
+			Places to Visit (Attractions):
+			Russia has a lot of popular sights in St. Petersburg to visit, such as:
+			The State Hermitage Museum
+			Peterhof Park and Palaces
+			Pavlovsk Park
+
+			Fun Things to Do (Entertainment):
+			The Bolshoi Theater offers world famous ballet and opera programs during all but summer months.
+			Extraordinary dramatic theaters, such as the Moscow Art Theater has performed plays by classic playwrights such as a Chekhov.
+			The world famous Obraztsov Puppet Theater are prime attractions for families. Both
+			Moscow Conservatory has concerts and recitals featuring Russia’s best musical performers.
+
+			Food:
+			Pelmeni -
+			It’s a type of pastry dumpling typically filled with meat. Several ways to serve it, slathered in butter, topped with sourcream or served in a broth.
+			Bilini -
+			If you’re a big fan of breakfast, you should try out Blini. They are thin pancakes like crepe and said to symbolize the sun- warm, round, and golden.
+			Vodka -
+			Who can forget, the main dish, Vodka.
+
+			Culture:
+			First thing that should come in mind is the Russian nesting dolls. They are well-known symbols of the country, the paintings of each doll, can be extremely elaborate and usually symbolizes a Russian peasant girl in traditional costume.
+
 		</div>
 		<div id="about" style="display:none;width:500px;">
 			<h1>Example Text</h1>
 			More text here i am
 		</div>
-		
+
 
 	</div>
