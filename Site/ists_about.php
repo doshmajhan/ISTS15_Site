00491f8c8d67155718b134803e738e91838fdd3e
eyy <lol@gmail.com>
2017-03-04 00:47:32 -0800

    Updating our website

--- Site/ists_about.php
+++ Site/ists_about.php
@@ -33,8 +33,8 @@
 		</div>
 		<div id="data">
 
-			<div id="page_data">
-				Your balance is: <div id="bal">Loading...</div>
+			<div id="page_data1" style="width:500px; text-align:center;margin:auto;margin-top:25px;">
+Welcome To Bank of SPARSA<br />You can buy many things with the money you make being a bank. Be sure to keep your services up so that you can make money! Each check is worth $7812.5 and 8 services are monitored for potential $62500 per round. Rounds are undertaken every 5 minutes. Remember that your money doesn't play into your overall score. If you have extra funds, please take a look at the wide verity of offerings we provide.
 			</div>
 
 		</div>
